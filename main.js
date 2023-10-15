bc63df4b821f057528602d0acc7ac941bfc9db9a
Real-Chaos <rizwannafees13@gmail.com>
2023-10-15 19:22:23 -0400

    added keyboard support

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // ---------------------- Declaring Variables ------------------ //
 
-
 const numbers = document.querySelectorAll('.number-btn')
 const operations = document.querySelectorAll('.operator')
 const display = document.querySelector('.calculator-display h1')
@@ -11,37 +10,41 @@ let firstNumber = ''
 let secondNumber = ''
 let operator = ''
 
-
 // ---------------------- Adding Event Listeners ------------------ //
 
 equals.addEventListener('click', () => {
   doOperations()
 })
 
-clear.addEventListener('click', () => {
+const clearCalculator = () => {
   firstNumber = ''
   secondNumber = ''
   operator = ''
   display.textContent = '0'
-})
+}
 
+clear.addEventListener('click', clearCalculator)
 
 // ---------------------- Declaring calculator functions ------------------ //
 
 const sum = (a, b) => {
-  return a + b
+  const addition = a + b
+  return +parseFloat(addition).toFixed( 6 )
 }
 
 const subtract = (a, b) => {
-  return a - b
+  const subtract = a - b
+  return +parseFloat(subtract).toFixed(6)
 }
 
 const multiply = (a, b) => {
-  return a * b
+  const multiply = a * b
+  return +parseFloat(multiply).toFixed(6)
 }
 
 const divide = (a, b) => {
-  return a / b
+  const division = a / b
+  return +parseFloat(division).toFixed(6)
 }
 
 const remainder = (a, b) => {
@@ -50,22 +53,61 @@ const remainder = (a, b) => {
 
 // ---------------------- updating Numbers ------------------ //
 
+const updateNumber = (num) => {
+  if (typeof firstNumber !== 'number' && operator === '') {
+    if (num === '.' && !firstNumber.includes('.')) {
+      firstNumber += num
+      display.textContent = firstNumber
+      return
+    } else if (num === '.' && firstNumber.includes('.')) return
+    firstNumber += num
+    display.textContent = firstNumber
+    console.log(firstNumber.includes('.'))
+  } else if (firstNumber === Number(firstNumber) && operator.length > 0) {
+    if (num === '.' && !secondNumber.includes('.')) {
+      secondNumber += num
+      display.textContent = secondNumber
+      return
+    } else if (num === '.' && secondNumber.includes('.')) return
+    if (secondNumber == 0) secondNumber = num
+    else secondNumber += num
+    display.textContent = secondNumber
+  }
+}
+
 const updatingNumbers = () => {
   numbers.forEach((num) => {
-    num.addEventListener('click', () => {
-      if (typeof firstNumber !== 'number' && operator === '') {
-        firstNumber += num.textContent
-        display.textContent = firstNumber
-      } else if (firstNumber === Number(firstNumber) && operator.length > 0) {
-        secondNumber += num.textContent
-        display.textContent = secondNumber
-      }
-    })
+    num.addEventListener('click', () => updateNumber(num.textContent))
   })
 }
 
-// ---------------------- Doing Operations ------------------ //
 
+// ---------------------- Keyboard support ------------------ //
+
+document.addEventListener('keydown', (e) => {
+  if (isFinite(e.key) || e.key===".") {
+    e.preventDefault()
+    updateNumber(e.key)
+  } else if (e.key === 'Backspace') {
+    clearCalculator()
+  } else if (e.key === '+') {
+    doOperations()
+    operator = 'add'
+  } else if (e.key === '-') {
+    doOperations()
+    operator = 'subtract'
+  } else if (e.key === '*') {
+    doOperations()
+    operator = 'multiply'
+  } else if (e.key === '/') {
+    doOperations()
+    operator = 'divide'
+  } else if (e.key === 'Enter') {
+    doOperations()
+  } 
+})
+
+// ---------------------- Doing Operations ------------------ //
 
 const doOperations = () => {
   firstNumber = Number(firstNumber)
@@ -111,7 +153,6 @@ const addSwitchingSigns = () => {
 }
 
 // ---------------------- Updating Operators ------------------ //
-
 
 const updatingOperator = () => {
   operations.forEach((operation) => {
