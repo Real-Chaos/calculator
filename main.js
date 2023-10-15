fee9308438d767fb1a9b698dc4b207ec76700445
Real-Chaos <rizwannafees13@gmail.com>
2023-10-15 18:06:29 -0400

    added all the functionality. moving onto refractoring code

--- main.js
+++ main.js
@@ -3,67 +3,11 @@ const operations = document.querySelectorAll('.operator')
 const display = document.querySelector('.calculator-display h1')
 const equals = document.querySelector('.equals')
 const clear = document.querySelector('.clear')
-let firstNumber = null
-let secondNumber = null
+const switchSigns = document.querySelector('.switch-signs')
+let firstNumber = ''
+let secondNumber = ''
 let operator = ''
 
-numbers.forEach((num) => {
-  num.addEventListener('click', () => {
-    display.textContent = num.textContent
-    if (firstNumber === null && operator === '') {
-      firstNumber = Number(num.textContent)
-    } else if (firstNumber === Number(firstNumber) && operator.length > 0) {
-      secondNumber = Number(num.textContent)
-    }
-  })
-})
-
-const cleanUp = (result) => {
-  display.textContent = result
-  firstNumber = result
-  secondNumber = null
-  operator = ''
-}
-
-const doOperations = () => {
-  if (operator === 'add') {
-    const result = sum(firstNumber, secondNumber)
-    cleanUp(result)
-  } else if (operator === 'subtract') {
-    const result = subtract(firstNumber, secondNumber)
-    cleanUp(result)
-  } else if (operator === 'multiply') {
-    const result = multiply(firstNumber, secondNumber)
-    cleanUp(result)
-  } else if (operator === 'divide') {
-    const result = divide(firstNumber, secondNumber)
-    cleanUp(result)
-  }
-}
-
-equals.addEventListener('click', () => {
-  doOperations()
-})
-
-clear.addEventListener('click', () => {
-  firstNumber = null
-  secondNumber = null
-  operator = ''
-  display.textContent = '0'
-})
-
-operations.forEach((operation) => {
-  operation.addEventListener('click', () => {
-    if (operator === '') {
-      operator = operation.classList[1]
-    } else if(operator.length > 0) {
-      console.log('do something')
-      console.log(`${firstNumber} ${operator} ${secondNumber}`)
-      doOperations()
-      operator = operation.classList[1]
-    }
-  })
-})
 const sum = (a, b) => {
   return a + b
 }
@@ -79,3 +23,87 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
   return a / b
 }
+
+const remainder = (a, b) => {
+  return a%b
+}
+
+numbers.forEach((num) => {
+  num.addEventListener('click', () => {
+    // display.textContent = num.textContent
+    if (typeof firstNumber !== 'number' && operator === '') {
+      firstNumber += num.textContent
+      display.textContent = firstNumber
+      // firstNumber = Number(num.textContent)
+    } else if (firstNumber === Number(firstNumber) && operator.length > 0) {
+      secondNumber += num.textContent
+      display.textContent = secondNumber
+      // secondNumber = Number(num.textContent)
+    }
+  })
+})
+
+const cleanUp = (result) => {
+  display.textContent = result
+  firstNumber = result
+  secondNumber = ''
+  operator = ''
+}
+
+const doOperations = () => {
+  firstNumber = Number(firstNumber)
+  secondNumber = Number(secondNumber)
+  if (operator === 'add') {
+    const result = sum(firstNumber, secondNumber)
+    cleanUp(result)
+  } else if (operator === 'subtract') {
+    const result = subtract(firstNumber, secondNumber)
+    cleanUp(result)
+  } else if (operator === 'multiply') {
+    const result = multiply(firstNumber, secondNumber)
+    cleanUp(result)
+  } else if (operator === 'divide') {
+    const result = divide(firstNumber, secondNumber)
+    cleanUp(result)
+  } else if(operator==='remainder') {
+    const result = remainder(firstNumber, secondNumber)
+    cleanUp(result)
+  }
+}
+
+switchSigns.addEventListener('click', () => {
+  if((firstNumber.length > 0 || typeof firstNumber === 'number')&& secondNumber.length < 1) {
+    firstNumber = Number(firstNumber)
+    firstNumber = firstNumber*-1
+    display.textContent = firstNumber
+  } else if(typeof firstNumber === 'number' && (secondNumber.length > 0|| typeof secondNumber === 'number')) {
+    secondNumber = Number(secondNumber)
+    secondNumber = secondNumber*-1
+    display.textContent = secondNumber
+  }
+})
+
+equals.addEventListener('click', () => {
+  doOperations()
+})
+
+clear.addEventListener('click', () => {
+  firstNumber = ''
+  secondNumber = ''
+  operator = ''
+  display.textContent = '0'
+})
+
+operations.forEach((operation) => {
+  operation.addEventListener('click', () => {
+    firstNumber = Number(firstNumber)
+    if (operator === '') {
+      operator = operation.classList[1]
+    } else if (operator.length > 0) {
+      console.log('do something')
+      console.log(`${firstNumber} ${operator} ${secondNumber}`)
+      doOperations()
+      operator = operation.classList[1]
+    }
+  })
+})
